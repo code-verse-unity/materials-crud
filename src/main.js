7b0a54d64dc1faa3587c7b04fa8153f3442f1a71
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-07-05 20:14:42 +0300

    chore: Init vue project

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import "./assets/main.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.esm.min.js";
+
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
