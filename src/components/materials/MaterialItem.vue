33ce7df6d7c914627cbdd7a5adf572856a017f94
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-07-08 14:23:24 +0300

    feat(EditMaterial.vue): Create a component for editing a material
    fix(MaterialItem.vue): Use the EditMaterial component
    fix(material.js): Change the updateMaterial function to be async

--- src/components/materials/MaterialItem.vue
+++ src/components/materials/MaterialItem.vue
@@ -4,13 +4,14 @@
     <td>{{ name }}</td>
     <td>{{ status }}</td>
     <td>{{ quantity }}</td>
-    <td></td>
+    <td><EditMaterial :id="id" :name="name" :status="status" :quantity="quantity" /></td>
     <td><DeleteMaterial :id="id" :name="name" :status="status" :quantity="quantity" /></td>
   </tr>
 </template>
 
 <script setup>
 import DeleteMaterial from "./DeleteMaterial.vue";
+import EditMaterial from "./EditMaterial.vue";
 
 const props = defineProps({
   id: {
